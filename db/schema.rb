ec1acd19c6faa71fe0a3a64e0b5bfa9a7fd77f6d
SUGINO Yasuhiro <suginoyasuhiro@gmail.com>
2013-10-05 22:58:35 +0900

    Add index to checks.created_at

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131005132739) do
+ActiveRecord::Schema.define(version: 20131005134320) do
 
   create_table "checks", force: true do |t|
     t.integer  "word_id"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20131005132739) do
     t.datetime "updated_at"
   end
 
+  add_index "checks", ["created_at"], name: "index_checks_on_created_at"
   add_index "checks", ["word_id"], name: "index_checks_on_word_id"
 
   create_table "clips", force: true do |t|
